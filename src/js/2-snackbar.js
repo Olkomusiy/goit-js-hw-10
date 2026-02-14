22c284bbf083acb47269aa29236d00adeaf5c751
Oleh Musii <oleg.musiy321@gmail.com>
2026-02-14 18:48:40 +0200

    123

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,38 @@
+const form = document.querySelector('form');
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const delay = event.target.elements.delay.value;
+    const delayNumber = Number(delay);
+    const state = event.target.elements.state.value;
+
+    console.log(delay, state);
+
+
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (state === "fulfilled") {
+                resolve(delayNumber);
+            } else {
+                reject(delayNumber);
+            }
+
+        }, delayNumber);
+
+    });
+    promise.then((value) => {
+        iziToast.success({
+            title: 'Success',
+            message: `✅ Fulfilled promise in ${value}ms`,
+            timeout: 5000
+        });
+    })
+    promise.catch((error) => {
+        iziToast.error({
+            title: 'Error',
+            message: `❌ Rejected promise in ${error}ms`,
+            timeout: 5000
+        });
+    });
+
+});
